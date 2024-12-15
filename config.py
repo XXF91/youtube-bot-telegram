4a1045e54a49e7ac2a1af6e91651f55f3c06f3e8
XXF91 <mrsjoaker@gmail.com>
2024-12-15 23:09:28 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ import os
 
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", 12345678))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    API_ID = int(os.environ.get("API_ID", 21254911))
+    API_HASH = os.environ.get("API_HASH","95e927b3c48ac0af4ebb1c3ffeb0069b")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN","7083760794:AAF_TNGEfpnwxkkKCDoOOj0bLarMMCf8sWo")
